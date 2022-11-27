26f11c9d9f417792f25588b50134d9bf269c4249
KPMGE <jesuskevin254@gmail.com>
2022-11-27 17:33:34 -0300

    :poop: dummy implementations, to be refactored

--- src/main.c
+++ src/main.c
@@ -1,7 +1,117 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
+#include "../include/priorityQueue.h"
+// #include "../include/network.h"
 
-int main (int argc, char *argv[])
-{
-  printf("hello world");
+typedef struct {
+  int origin_id, destination_id;
+  double weight;
+} Edge;
+
+typedef struct {
+
+} Graph;
+
+typedef struct {
+  int qtd_vertices, qtd_edges, qtd_servers, qtd_clients, qtd_monitors;
+  int *servers, *clients, *monitors;
+  Edge **edges;
+} Network;
+
+Network *read_net(const char *file_path);
+Edge *edge_new(int origin_id, int destination_id, double weight);
+int edge_origin_id(const Edge *e);
+int edge_destination_id(const Edge *e);
+double edge_weight(const Edge *e);
+double edge_find_weight(const Edge **edges, int qtd_edges, int origin_id, int destination_id);
+double *dijkstra(const Edge **edges, const int *vertices, int graph_size, int source);
+
+int main(int argc, char *argv[]) {
+  assert(argc >= 2);
+  Network *n = read_net(argv[1]);
+
+  printf("|V|: %d, |E|: %d\n", n->qtd_vertices, n->qtd_edges);
+  printf("|S|: %d, |C|: %d, |M|: %d\n", n->qtd_servers, n->qtd_clients,
+         n->qtd_monitors);
+  printf("\n");
+
+  printf("\nservers: \n");
+  for (int i = 0; i < n->qtd_servers; i++) {
+    printf("%d\n", n->servers[i]);
+  }
+
+  printf("\nclients: \n");
+  for (int i = 0; i < n->qtd_clients; i++) {
+    printf("%d\n", n->clients[i]);
+  }
+
+  printf("\nmonitors: \n");
+  for (int i = 0; i < n->qtd_monitors; i++) {
+    printf("%d\n", n->monitors[i]);
+  }
+
+  printf("\nedges:\n");
+  for (int i = 0; i < n->qtd_edges; i++) {
+    printf("%d->%d: %lf\n", edge_origin_id(n->edges[i]), edge_destination_id(n->edges[i]), edge_weight(n->edges[i]));
+  }
+}
+
+Edge *edge_new(int origin_id, int destination_id, double weight) {
+  Edge *e = malloc(sizeof(Edge));
+  e->origin_id = origin_id;
+  e->destination_id = destination_id;
+  e->weight = weight;
+  return e;
+}
+
+int edge_origin_id(const Edge *e) { return e->origin_id; }
+int edge_destination_id(const Edge *e) { return e->destination_id; }
+double edge_weight(const Edge *e) { return e->weight; }
+double edge_find_weight(const Edge **edges, int qtd_edges, int origin_id, int destination_id) {
+  for (int i = 0; i < qtd_edges; i++) {
+    if (edge_origin_id(edges[i]) == origin_id && edge_destination_id(edges[i]) == destination_id) {
+      return edge_weight(edges[i]);
+    }
+  }
+  return -1;
+}
+
+Network *read_net(const char *file_path) {
+  FILE *file = fopen(file_path, "r");
+  assert(file != NULL && "Could not open file");
+
+  Network *n = malloc(sizeof(Network));
+
+  fscanf(file, "%d %d", &n->qtd_vertices, &n->qtd_edges);
+  fscanf(file, "%d %d %d", &n->qtd_servers, &n->qtd_clients, &n->qtd_monitors);
+
+  n->edges = malloc(sizeof(Edge) * n->qtd_edges);
+  n->clients = malloc(sizeof(int) * n->qtd_clients);
+  n->servers = malloc(sizeof(int) * n->qtd_servers);
+  n->monitors = malloc(sizeof(int) * n->qtd_monitors);
+
+  for (int i = 0; i < n->qtd_servers; i++) {
+    fscanf(file, "%d", &n->servers[i]);
+  }
+
+  for (int i = 0; i < n->qtd_clients; i++) {
+    fscanf(file, "%d", &n->clients[i]);
+  }
+
+  for (int i = 0; i < n->qtd_monitors; i++) {
+    fscanf(file, "%d", &n->monitors[i]);
+  }
+
+  int x, y;
+  double weight;
+  for (int i = 0; i < n->qtd_edges; i++) {
+    fscanf(file, "%d %d %lf", &x, &y, &weight);
+    n->edges[i] = edge_new(x, y, weight);
+  }
+
+  fclose(file);
+
+  return n;
 }
---
+++ src/pq.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/priorityQueue.h"
+#include "../include/vertex.h"
+
+struct priorityQueue {
+  Vertex **array;
+  int *map;
+  int quantity;
+};
+
+void swap(PriorityQueue *p, int i, int j) {
+  vertex_exch(&p->array[i], &p->array[j]);
+  p->map[vertex_id(p->array[i])] = i;
+  p->map[vertex_id(p->array[j])] = j;
+}
+
+void fix_up(PriorityQueue *p, int k) {
+  while (k > 1 && vertex_more(p->array[k / 2], p->array[k])) {
+    swap(p, k, k / 2);
+    k = k / 2;
+  }
+}
+
+void fix_down(PriorityQueue *p, int k) {
+  while (2 * k <= p->quantity) {
+    int j = 2 * k;
+    if (j < p->quantity && vertex_more(p->array[j], p->array[j + 1])) {
+      j++;
+    }
+    if (!vertex_more(p->array[k], p->array[j])) {
+      break;
+    }
+    swap(p, k, j);
+    k = j;
+  }
+}
+
+PriorityQueue *PriorityQueue_init(int maxN) {
+  PriorityQueue *pq = (PriorityQueue *)malloc(sizeof(PriorityQueue));
+  pq->array = (Vertex **)malloc((maxN + 1) * sizeof(Vertex *));
+  pq->map = (int *)malloc((maxN + 1) * sizeof(int));
+  pq->quantity = 0;
+  return pq;
+}
+
+void PriorityQueue_insert(PriorityQueue *p, Vertex *v) {
+  p->quantity++;
+  p->array[p->quantity] = v;
+  p->map[vertex_id(v)] = p->quantity;
+  fix_up(p, p->quantity);
+}
+
+Vertex *PriorityQueue_delmin(PriorityQueue *p) {
+  Vertex *min = p->array[1];
+  swap(p, 1, p->quantity);
+  p->quantity--;
+  fix_down(p, 1);
+  return min;
+}
+
+Vertex *PriorityQueue_min(PriorityQueue *p) { return p->array[1]; }
+
+void PriorityQueue_decrease_key(PriorityQueue *p, int id, double value) {
+  int i = p->map[id];
+  vertex_set_value(p->array[i], value);
+  fix_up(p, i);
+}
+
+bool PriorityQueue_empty(PriorityQueue *p) { return p->quantity == 0; }
+
+int PriorityQueue_size(PriorityQueue *p) { return p->quantity; }
+
+void PriorityQueue_finish(PriorityQueue *p) {
+  free(p->array);
+  free(p->map);
+  free(p);
+}
