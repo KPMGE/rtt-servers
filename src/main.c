ef50f9d3860d7a2b75f2196640425f8c2221e4cb
KPMGE <jesuskevin254@gmail.com>
2022-11-30 13:10:45 -0300

    :recycle: first full-working implementation

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
-#include "../include/priorityQueue.h"
 #include "../include/graph.h"
 #include "../include/network.h"
 #include "../include/utils.h"
@@ -12,29 +11,29 @@ int main(int argc, char *argv[]) {
   assert(argc >= 2);
   Network *n = read_net(argv[1]);
 
-  printf("|V|: %d, |E|: %d\n", n->qtd_vertices, n->qtd_edges);
-  printf("|S|: %d, |C|: %d, |M|: %d\n", n->qtd_servers, n->qtd_clients,
-         n->qtd_monitors);
-  printf("\n");
+  // printf("|V|: %d, |E|: %d\n", n->qtd_vertices, n->qtd_edges);
+  // printf("|S|: %d, |C|: %d, |M|: %d\n", n->qtd_servers, n->qtd_clients,
+  //        n->qtd_monitors);
+  // printf("\n");
 
-  printf("\nservers: \n");
-  for (int i = 0; i < n->qtd_servers; i++) {
-    printf("%d\n", n->servers[i]);
-  }
+  // printf("\nservers: \n");
+  // for (int i = 0; i < n->qtd_servers; i++) {
+  //   printf("%d\n", n->servers[i]);
+  // }
 
-  printf("\nclients: \n");
-  for (int i = 0; i < n->qtd_clients; i++) {
-    printf("%d\n", n->clients[i]);
-  }
+  // printf("\nclients: \n");
+  // for (int i = 0; i < n->qtd_clients; i++) {
+  //   printf("%d\n", n->clients[i]);
+  // }
 
-  printf("\nmonitors: \n");
-  for (int i = 0; i < n->qtd_monitors; i++) {
-    printf("%d\n", n->monitors[i]);
-  }
+  // printf("\nmonitors: \n");
+  // for (int i = 0; i < n->qtd_monitors; i++) {
+  //   printf("%d\n", n->monitors[i]);
+  // }
 
-  printf("\nGraph: \n");
-  print_graph(n->graph);
-
+  // printf("\nGraph: \n");
+  // print_graph(n->graph);
+  
   for (int i = 0; i < n->qtd_servers; i++) {
     for (int j = 0; j < n->qtd_clients; j++){
       const int server_id = n->servers[i];
@@ -44,7 +43,7 @@ int main(int argc, char *argv[]) {
       const double estimated_rtt = calculate_estimated_rtt(n->graph, server_id, client_id, n->monitors, n->qtd_monitors);
       const double inflation = estimated_rtt / real_rtt;
 
-      printf("%d %d %.2f\n", server_id, client_id, inflation);
+      printf("%d %d %.16f\n", server_id, client_id, inflation);
     }
   }
 }
--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,5 @@
 #include "../include/utils.h"
-#include "../include/priorityQueue.h"
+#include "../include/priority_queue.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -56,30 +56,31 @@ double *dijkstra(Graph* graph, int src) {
 		visited[i] = 0;
 	}
 	distance[src] = 0.0;
-  PriorityQueue *pq = PriorityQueue_init(amount_vertices);
+  PriorityQueue *pq = priority_queue_init(amount_vertices);
   Vertex *new_node = vertex_new(src);
   new_node->weight = 0.0;
 
-  PriorityQueue_insert(pq, new_node);
+  priority_queue_insert(pq, new_node);
 
-	while (!PriorityQueue_empty(pq))  {
-		Vertex *current = PriorityQueue_delmin(pq);
+	while (!priority_queue_empty(pq))  {
+		Vertex *current = priority_queue_delmin(pq);
 		int index = current->id;
 		visited[index] = 1;
 
 		double new_weight;
     Vertex *temp = graph_get_vertex(graph, index);
+
     while (temp) {
 			if (visited[temp->id] == 0) {
 				new_weight = distance[index] + temp->weight;
 				if (new_weight < distance[temp->id]) {
 					distance[temp->id] = new_weight;
-					if(PriorityQueue_contains(pq, temp->id)){
-            PriorityQueue_decrease_key(pq, temp->id, new_weight);
+					if(priority_queue_contains(pq, temp->id)){
+            priority_queue_decrease_key(pq, temp->id, new_weight);
 					} else {
 						Vertex *newNode = vertex_new(temp->id);
 						newNode->weight = new_weight;
-            PriorityQueue_insert(pq, newNode);
+            priority_queue_insert(pq, newNode);
 					}
 				}
 			}
@@ -87,6 +88,7 @@ double *dijkstra(Graph* graph, int src) {
     }
 	}
 
+  priority_queue_finish(pq);
   free(visited);
 	return distance;
 }
