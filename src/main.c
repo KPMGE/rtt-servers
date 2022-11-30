a7b2e523d3ce5b93737f7630ead9195996d6bda0
KPMGE <jesuskevin254@gmail.com>
2022-11-30 14:38:40 -0300

    :recycle: encapsulate network structure

--- src/main.c
+++ src/main.c
@@ -9,38 +9,20 @@
 
 int main(int argc, char *argv[]) {
   assert(argc >= 2);
-  Network *n = read_net(argv[1]);
+  Network *n = read_network(argv[1]);
+  int *servers = network_servers(n);
+  int *clients = network_clients(n);
+  int *monitors = network_monitors(n);
+  Graph *graph = network_graph(n);
+  int qtd_monitors = network_qtd_monitors(n);
 
-  // printf("|V|: %d, |E|: %d\n", n->qtd_vertices, n->qtd_edges);
-  // printf("|S|: %d, |C|: %d, |M|: %d\n", n->qtd_servers, n->qtd_clients,
-  //        n->qtd_monitors);
-  // printf("\n");
+  for (int i = 0; i < network_qtd_servers(n); i++) {
+    for (int j = 0; j < network_qtd_clients(n); j++){
+      const int server_id = servers[i];
+      const int client_id = clients[j];
 
-  // printf("\nservers: \n");
-  // for (int i = 0; i < n->qtd_servers; i++) {
-  //   printf("%d\n", n->servers[i]);
-  // }
-
-  // printf("\nclients: \n");
-  // for (int i = 0; i < n->qtd_clients; i++) {
-  //   printf("%d\n", n->clients[i]);
-  // }
-
-  // printf("\nmonitors: \n");
-  // for (int i = 0; i < n->qtd_monitors; i++) {
-  //   printf("%d\n", n->monitors[i]);
-  // }
-
-  // printf("\nGraph: \n");
-  // print_graph(n->graph);
-  
-  for (int i = 0; i < n->qtd_servers; i++) {
-    for (int j = 0; j < n->qtd_clients; j++){
-      const int server_id = n->servers[i];
-      const int client_id = n->clients[j];
-
-      const double real_rtt = rtt(n->graph, server_id, client_id);
-      const double estimated_rtt = calculate_estimated_rtt(n->graph, server_id, client_id, n->monitors, n->qtd_monitors);
+      const double real_rtt = rtt(graph, server_id, client_id);
+      const double estimated_rtt = calculate_estimated_rtt(graph, server_id, client_id, monitors, qtd_monitors);
       const double inflation = estimated_rtt / real_rtt;
 
       printf("%d %d %.16f\n", server_id, client_id, inflation);
--- src/utils.c
+++ src/utils.c
@@ -7,45 +7,6 @@
 
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 
-Network *read_net(const char *file_path) {
-  FILE *file = fopen(file_path, "r");
-  assert(file != NULL && "Could not open file");
-
-  Network *n = malloc(sizeof(Network));
-
-  fscanf(file, "%d %d", &n->qtd_vertices, &n->qtd_edges);
-  fscanf(file, "%d %d %d", &n->qtd_servers, &n->qtd_clients, &n->qtd_monitors);
-
-  n->clients = malloc(sizeof(int) * n->qtd_clients);
-  n->servers = malloc(sizeof(int) * n->qtd_servers);
-  n->monitors = malloc(sizeof(int) * n->qtd_monitors);
-  n->graph = graph_new(n->qtd_vertices);
-
-  for (int i = 0; i < n->qtd_servers; i++) {
-    fscanf(file, "%d", &n->servers[i]);
-  }
-
-  for (int i = 0; i < n->qtd_clients; i++) {
-    fscanf(file, "%d", &n->clients[i]);
-  }
-
-  for (int i = 0; i < n->qtd_monitors; i++) {
-    fscanf(file, "%d", &n->monitors[i]);
-  }
-
-  int x, y;
-  double weight;
-  for (int i = 0; i < n->qtd_edges; i++) {
-    fscanf(file, "%d %d %lf", &x, &y, &weight);
-    add_edge(n->graph, x, y, weight);
-  }
-
-  fclose(file);
-
-  return n;
-}
-
-
 double *dijkstra(Graph* graph, int src) {
   int amount_vertices = graph_vertices(graph);
 	double *distance = malloc(sizeof(double) * amount_vertices);
